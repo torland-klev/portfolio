4900c9d24802371d07328b1164d7dc015b831d0f
klevhenrik@gmail.com <Henrik Klev>
2022-07-17 15:29:13 +0200

    Finished portfolio

--- src/components/pages/PortfolioPage.tsx
+++ src/components/pages/PortfolioPage.tsx
@@ -6,6 +6,7 @@ import ztl from '../../images/ztl.png'
 import payex from '../../images/payex.png'
 import technicalDebt from '../../images/technical-debt.png'
 import tripletexIntegrations from '../../images/tripletex-integrations.png'
+import uio from '../../images/uio.png'
 import { wait } from '@testing-library/user-event/dist/utils'
 
 export default function PortfolioPage() {
@@ -39,42 +40,175 @@ function PortfolioTitle() {
 }
 
 function PortfolioBody() {
+    const tripletexTags = [
+        'Java',
+        'React',
+        'TypeScript',
+        'React Redux',
+        'React Saga',
+        'React Router',
+        'Maria DB',
+        'AWS',
+        'SQL',
+        'Grafana',
+        'Prometheus',
+        'Cypress',
+        'Spring',
+        'ORM',
+        'Maven',
+        'Docker',
+        'IntelliJ',
+        'Tomcat',
+        'Webpack',
+        'Vite',
+        'Nods.js',
+        'npm',
+        'Jenkins',
+        'Kibana',
+        'sass',
+        'JSP',
+    ]
+
+    const tripletexBasic =
+        'Tripletex is one of the leading accounting systems in Norway, and has been operational for more than 20 years. ' +
+        'As part of the Piggy Bank Crew, the team responsible for banking and bank integrations, I partook in a couple of projects. ' +
+        'The team consisted of a handful developers, a QA and a UX designer.'
+
     return (
         <div className={styles.portfolioBody}>
             <PortfolioCard
                 image={payex}
                 company={'PayEx'}
-                project={'PAX30 payment terminal'}
-                text={'foo'}
+                project={'Android Payment Terminal'}
+                role={'Android developer'}
+                text={
+                    'PayEx is a leading nordic payment provider that provides complete payment solutions for stores and e-commerce. ' +
+                    'As part of an agile team consisting of 10 developers, 3 QA and team lead, ' +
+                    'I helped develop a new line of stand-alone PAX30 payment terminals. The terminals are to be ' +
+                    'deployed to major retailers, such as Coop, Volvo, Circle K, Narvesen, 7-Eleven, and others by Q4 2022. ' +
+                    'The terminals where written in Android using Kotlin, Jetpack Compose and Dagger, with ' +
+                    'a test-suite utilizing Mockito and JUnit 4.'
+                }
                 period={'May 2022 - Ongoing'}
+                tags={[
+                    'Kotlin',
+                    'Android',
+                    'Jetpack Compose',
+                    'Dagger',
+                    'Mockito',
+                    'JUnit 4',
+                    'Gradle',
+                    'Agile',
+                    'Fintech',
+                    'Git',
+                    'Jira',
+                    'GitHub',
+                    'Nexo Acquirer',
+                    'Docker',
+                    'IntelliJ',
+                    'Android Studio',
+                ]}
             />
             <PortfolioCard
                 image={ztl}
-                company={'ZTL Payment Solution AS'}
-                project={'PSD2 B2B payment solution'}
-                text={'foo'}
-                period={'November 2021 - May 2022'}
+                company={'ZTL Payment Solution'}
+                project={'PSD2 B2B Payment Solution'}
+                role={'Fullstack developer'}
+                text={
+                    'ZTL Payment Solution is a fintech scale-up that utilizes the PSD2 APIs of financial instituions ' +
+                    'to provide real-time payment solutions and account services. During my time there, I helped develop their' +
+                    ' REST API-based payment solution and internal management system. The payment solution was written in Kotlin ' +
+                    'using KTOR, JDBI and PSQL, hosted on MicroSoft Azure, while their management system was React+TypeScript-based.'
+                }
+                period={'Nov 2021 - May 2022'}
+                tags={[
+                    'Kotlin',
+                    'React',
+                    'TypeScript',
+                    'Fintech',
+                    'Git',
+                    'Jira',
+                    'Bitbucket',
+                    'PSD2',
+                    'ISO 20022',
+                    'New Relic',
+                    'Grafana',
+                    'PSQL',
+                    'MicroSoft Azure',
+                    'Ktor',
+                    'Docker',
+                    'IntelliJ',
+                    'Nods.js',
+                    'npm',
+                    'sass',
+                ]}
             />
             <PortfolioCard
                 image={tripletexIntegrations}
                 company={'Tripletex'}
-                project={'Sbanken integration'}
-                text={'foo'}
-                period={'May 2021 - November 2021'}
+                project={'Sbanken Integration'}
+                role={'Fullstack developer, Team security lead'}
+                text={
+                    tripletexBasic +
+                    '\n\nMy final project at Tripletex consisted of integration towards the PSD2 API of Sbanken.' +
+                    'As a result, all customers that had an account in Sbanken, would seamlessly receive all their account information and payments directly into ' +
+                    'Tripletex. This gave customers full control over their accounts, enabled automatic reconciliation and closing of invoices, and enabled them ' +
+                    'to pay their invoices directly from Tripletex.'
+                }
+                period={'May 2021 - Nov 2021'}
+                tags={tripletexTags}
             />
             <PortfolioCard
                 image={technicalDebt}
                 company={'Tripletex'}
-                project={'Technical debt'}
-                text={'foo'}
+                project={'Technical Debt'}
+                role={'Fullstack developer, Team security lead, Frontend lead'}
+                text={
+                    tripletexBasic +
+                    '\n\nMy second project at Tripletex was focused on removing technical debt. ' +
+                    'When a system becomes as large, old and complex as that of Tripletex, you debt just starts piling up and ' +
+                    'the latency goes through the roof. By systematically analyzing the system in its multi-threaded setting, ' +
+                    'and exposing the bottlenecks that caused the most perceived latency, some modules became as ' +
+                    'much as 15 times more efficient.'
+                }
                 period={'Feb 2021 - May 2021'}
+                tags={tripletexTags}
             />
             <PortfolioCard
                 image={tripletex}
                 company={'Tripletex'}
-                project={'Incoming payments'}
-                text={'foo'}
-                period={'Okt 2020 - Feb 2021'}
+                project={'Incoming Payments'}
+                role={'Fullstack developer'}
+                text={
+                    tripletexBasic +
+                    '\n\nMy first project at Tripletex consisted of automatically fetching, parsing, posting and reconciling incoming payments. ' +
+                    'When the project was finished, customers received all the payments for their accounts automatically into their accounting systems, ' +
+                    'where all the payments were closed to the correct invoice and reconciled towards the corresponding bank-statement entry.'
+                }
+                period={'Oct 2020 - Feb 2021'}
+                tags={tripletexTags}
+            />
+            <PortfolioCard
+                image={uio}
+                company={'University of Oslo'}
+                project={'M. Sc.'}
+                role={'Researcher'}
+                text={
+                    "To receive my Master's degree in Computer Science, I focused in formally verifying parts of the " +
+                    'official Norwegian electoral system, EVA. By use of deductive verification and dynamic logic, I was able to ' +
+                    'mathematically prove that certain core components of the system both adhered to the program specification and that' +
+                    ' it would never fail to do so. The project was completed with guidance of the Reliable Systems group at the University of Oslo ' +
+                    'and the Formal Methods division at University of Gothenburg.'
+                }
+                period={'Aug 2018 - Jun 2020'}
+                tags={[
+                    'Java',
+                    'JUnit 4',
+                    'Deductive verification',
+                    'Formal methods',
+                    'Dynamic logic',
+                    'KeY',
+                ]}
             />
         </div>
     )
@@ -84,14 +218,18 @@ function PortfolioCard({
     image,
     company,
     project,
+    role,
     text,
     period,
+    tags,
 }: {
     image: string
     company: string
     project: string
+    role: string
     text: string
     period: string
+    tags: string[]
 }) {
     const [popoutVisible, setPopoutVisible] = useState(false)
 
@@ -100,9 +238,11 @@ function PortfolioCard({
             company={company}
             project={project}
             text={text}
+            role={role}
             visible={popoutVisible}
             onClose={() => setPopoutVisible(false)}
             period={period}
+            tags={tags}
         />
     )
 
@@ -131,16 +271,20 @@ function PopoutCard({
     company,
     project,
     text,
+    role,
     visible,
     onClose,
     period,
+    tags,
 }: {
     company: string
     project: string
     text: string
+    role: string
     visible?: boolean
     onClose: () => void
     period: string
+    tags: string[]
 }) {
     const [closing, setClosing] = useState(false)
 
@@ -150,6 +294,10 @@ function PopoutCard({
             setClosing(false)
             onClose()
         })
+    }
+
+    function Tag({ tag }: { tag: string }) {
+        return <div className={styles.tag}>{tag}</div>
     }
 
     return (
@@ -174,12 +322,24 @@ function PopoutCard({
                             x
                         </button>
                         <div className={styles.popoutCardBody}>
-                            <div className={styles.project}>{project}</div>
+                            <div className={styles.popoutCardBodyProject}>
+                                {project}
+                            </div>
                             <div className={styles.companyAndPeriod}>
                                 <div>{company}</div>
+                                <div>{role}</div>
                                 <div>{period}</div>
                             </div>
-                            <div className={styles.text}>{text}</div>
+                            <div className={styles.popoutCardBodyText}>
+                                {text}
+                            </div>
+                            {tags.length > 0 && (
+                                <div className={styles.popoutCardBodyTags}>
+                                    {tags.sort().map((tag) => (
+                                        <Tag key={tag} tag={tag} />
+                                    ))}
+                                </div>
+                            )}
                         </div>
                     </div>
                 </div>
